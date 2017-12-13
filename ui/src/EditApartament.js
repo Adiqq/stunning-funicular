88c8f26399521d0d75b5feb6998bef4891f1ef48
Adrian Wawrzak <Adiqq@users.noreply.github.com>
2017-12-13 17:21:24 +0100

    Copy exisiting files from another project

---
+++ ui/src/EditApartament.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './EditApartament.css';
+import EditApartmentForm from './EditApartamentForm';
+
+class EditApartament extends Component {
+  render() {
+    return <EditApartmentForm />;
+  }
+}
+
+export default EditApartament;
